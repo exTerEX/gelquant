4552d32c019130da4e4ce3ddf48f9f5c2ae7203b
exTerEX <30706568+exTerEX@users.noreply.github.com>
2020-10-15 10:30:24 +0200

    Modified gelquant/cli.py; PEP8 and docstring

--- gelquant/cli.py
+++ gelquant/cli.py
@@ -1,12 +1,16 @@
+"""Command-line Interface to interact with Gelquant."""
+
 import argparse
 
-def cli():
 
+def cli():
+    """Command-line interface main method."""
     parser = argparse.ArgumentParser()
     parser.add_argument('test')
 
     args = parser.parse_args()
     print(args.test)
+
 
 if __name__ == "__main__":
     cli()
