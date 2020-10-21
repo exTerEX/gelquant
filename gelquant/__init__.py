606be36e05e52ad112ab84285629e2f8623998be
exTerEX <30706568+exTerEX@users.noreply.github.com>
2020-10-21 20:34:25 +0200

    Formatted data, fixed methods and some patches in gelquant

--- gelquant/__init__.py
+++ gelquant/__init__.py
@@ -1,3 +1,4 @@
-from .gelquant import (aggregate_plotter, aggregator, area_integrator,
-                       fancy_plotter, half_life_calculator, image_cropping,
+from .gelquant import (area_integrator,
+                       fancy_plotter, image_cropping,
                        lane_parser, summary_data)
+from .__version__ import __version__
